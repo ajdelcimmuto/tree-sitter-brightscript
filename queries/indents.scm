bc2379ab3a2aaf0ba5ee548d69328cb2c7fc99f8
ajdelcimmuto <ajdelco22@gmail.com>
2024-08-10 10:15:44 -0500

    removing unneccessary indent rules

--- queries/indents.scm
+++ queries/indents.scm
@@ -34,5 +34,5 @@
 ; Ignore comments for indentation
 (comment) @indent.ignore
 
-(array ((_) @indent.align ","? @indent.end) @indent.begin)
-(assoc_array ((assoc_array_element) @indent.align ","? @indent.end) @indent.begin)
+;(array ((_) @indent.align ","? @indent.end) @indent.begin)
+;(assoc_array ((assoc_array_element) @indent.align ","? @indent.end) @indent.begin)
